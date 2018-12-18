f9e6bc024737e3330308d98767a247023c4e19f4
唐洵 <tangxun_123@163.com>
2018-12-18 18:38:43 +0800

    modify some button text and will add recyclerView example

---
+++ app/src/main/java/tank/com/kotlin/RecyclerViewActivity.kt
@@ -0,0 +1,13 @@
+package tank.com.kotlin
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * tank.com.kotlin <br/>
+ * Created by tank325 on 2018/12/18/6:36 PM.
+ */
+class RecyclerViewActivity : AppCompatActivity(){
+    override fun onCreateDescription(): CharSequence {
+        return super.onCreateDescription()
+    }
+}
