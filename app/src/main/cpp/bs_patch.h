fd2d54ed5e19333a4e3c0600717feff870993741
tank <tangxun_123@163.com>
2019-11-13 12:34:38 +0800

    更新jcenter/google仓库到aliyun

---
+++ app/src/main/cpp/bs_patch.h
@@ -0,0 +1,16 @@
+//
+// Created by vancetang on 2019/9/26.
+//
+
+#ifndef KOTLIN_TOTURIAL_BS_PATCH_H
+#define KOTLIN_TOTURIAL_BS_PATCH_H
+#include "tank_com_kotlin_ndk_BsPatchApi.h"
+
+class bs_patch {
+
+
+};
+
+
+
+#endif //KOTLIN_TOTURIAL_BS_PATCH_H
